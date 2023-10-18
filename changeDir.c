fbc762f837b418ae183a891c6ef660a164722538
Emmanuel <emmanuelmark501@gmail.com>
2023-10-18 08:02:50 -0700

    implenting change directory

---
+++ changeDir.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+/**
+ * changeDirectory - function to handle change directory
+ * @var: array of commands
+ * Return: return 0 on success and -1 if otherwise
+ */
+
+int changeDirectory(char **var)
+{
+	return (0);
+}
